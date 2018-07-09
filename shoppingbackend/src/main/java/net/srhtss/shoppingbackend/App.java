7cffaea160f328221d22916d4ba109a0cfdb9991
SERHAT ŞOS <serhad12365@gmail.com>
2018-07-09 15:41:08 +0300

    Proje başarıyla kuruldu.

---
+++ shoppingbackend/src/main/java/net/srhtss/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.srhtss.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
